54f1446e5dc7029314df8afe923d4cabe268ad66
cnsheafe <cnsheafe@gmail.com>
2017-11-10 14:39:11 -0700

    added test for retrieving statements

--- test/server.test.js
+++ test/server.test.js
@@ -15,7 +15,6 @@ describe("Generate random statement", function () {
   });  
 
   after(function () {
-
     return closeServer();
   });
 
@@ -25,6 +24,30 @@ describe("Generate random statement", function () {
       .then(function (res) {
         res.should.have.status(200);
         res.text.should.equal("Finished!");
+      })
+      .catch(function (err) {
+        throw err;
+      });
+  });
+});
+
+describe("Retrive statements from LRS", function () {
+  before(function () {
+    return openServer();
+  });  
+
+  after(function () {
+    return closeServer();
+  });
+
+  it("should successfully get a list of statements with GET /feed", function () {
+    return chai.request(app)
+      .get("/feed")
+      .then(function (res) {
+        res.should.have.status(200);
+        const body = JSON.parse(res.text);
+        body.should.have.property("statements");
+        body.statements.should.have.length.above(0);
       })
       .catch(function (err) {
         throw err;
